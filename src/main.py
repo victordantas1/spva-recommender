ba5154666c832f74a305a97fc87a53ada32f634c
victordantas1 <victorr.dantas@gmail.com>
2025-08-02 19:40:29 -0400

    feat: add cors policy

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from src.models.candidate import Candidate
 from src.models.job import Job
@@ -12,5 +13,17 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(title="SPVA Recommender", description="Recommendation of candidates API", lifespan=lifespan)
+
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(recommendation_router, tags=["recommendation"])
